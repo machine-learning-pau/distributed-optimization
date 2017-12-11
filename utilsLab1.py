fcdabec1f4e5e9a7aeeac282fafa66cd85193c3c
Yann Vernaz <Yann@Yann-Vernazs-MacBook-Pro-2.local>
2017-12-11 18:32:53 +0100

    add confusion matrix

--- utilsLab1.py
+++ utilsLab1.py
@@ -11,6 +11,7 @@ import os
 import csv
 import numpy as np
 from random import sample
+import itertools
 import matplotlib.pyplot as plt
 
 def data_generate(fileName='data.csv', w=[1., 2., -1], n=100):
@@ -121,7 +122,7 @@ def sigmoid(z):
     '''Sigmoid function'''
     return 1 / (1 + np.exp(-z))
 
-def prediction(x,w,threshold):
+def prediction(x,w,threshold=0.5):
     '''Prediction function'''
     if (sigmoid(-np.dot(x,w))>threshold):
         pred = -1.
@@ -338,4 +339,39 @@ def data_plot_solution(x, y, truew, algo, lambda1=0.0, lambda2=0.0):
     plt.ylabel("x2")
     plt.grid()
     plt.show()
+
+# source: http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html#sphx-glr-auto-examples-model-selection-plot-confusion-matrix-py
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+        This function prints and plots the confusion matrix.
+        Normalization can be applied by setting `normalize=True`.
+        """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+    
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
--- utilsLab2.py
+++ utilsLab2.py
@@ -12,6 +12,13 @@ import numpy as np
 from operator import add
 from pyspark.mllib.regression import LabeledPoint
 
+def modifLabel(x):
+    '''(Re)encoding the label (target variable)'''
+    y = -1.
+    if (x<0.5):
+        y = 1.
+    return y
+
 def data_read(sc, fileName = "retail.csv"):
     '''Reads the dataset (CSV format) and forms a labePoint RDD'''
     
@@ -22,8 +29,29 @@ def data_read(sc, fileName = "retail.csv"):
         retailRDD = sc.textFile(fileName).map(lambda line: line.split(','))
         header = retailRDD.first()
         retailRDD = retailRDD.filter(lambda line: line != header)
-        labelPointRDD = retailRDD.map(lambda line: LabeledPoint(line[0],[line[2:]]))
+        labelPointRDD = retailRDD.map(lambda line: LabeledPoint(modifLabel(float(line[0])),[line[2:]]))
     return labelPointRDD
+
+def data_read_undersampling(sc, fileName = "retail.csv"):
+    '''Reads the dataset (CSV format) and forms a labePoint RDD
+        
+        Stratified Sampling : we're keeping all instances of the Churn=True class,
+        but downsampling the Churn=False class to 0.1 fraction.
+        '''
+    
+    if os.path.isfile(fileName) != True:
+        print("Le fichier de donnés "+ fileName + " n'existe pas.")
+        return
+    else:
+        retailRDD = sc.textFile(fileName).map(lambda line: line.split(','))
+        header = retailRDD.first()
+        retailRDD = retailRDD.filter(lambda line: line != header)
+        underSamplingRDD = retailRDD.map(lambda line: (modifLabel(float(line[0])),line[2:]))
+        # specify the exact fraction desired from each key as a dictionary
+        fractions = {1: 0.1, -1: 1.0}
+        underSamplingRDD = underSamplingRDD.sampleByKey(False, fractions)
+        underSamplingRDD = underSamplingRDD.map(lambda line: LabeledPoint(line[0],line[1]))
+    return underSamplingRDD
 
 def data_scaled(labelPointRDD):
     '''Normalizes features with mean 0 and std 1.'''
